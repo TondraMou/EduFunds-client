64fc212a9827d60e90e3a1423987085047fb02cd
TondraMou <tondramou3@gmail.com>
2025-01-24 22:03:32 +0600

    added user menu

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
+import DashboardLayout from '../layouts/DashboardLayout'
 
 export const router = createBrowserRouter([
-  
+  {
+    path: '/dashboard',
+    element: <DashboardLayout></DashboardLayout>
+  }
 ])
