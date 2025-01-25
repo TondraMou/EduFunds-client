ff943743ae70c4dc67e540ea8ac0f07f3980538f
TondraMou <tondramou3@gmail.com>
2025-01-25 16:09:41 +0600

    added navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Shared/Navbar/Navbar'
 
 const MainLayout = () => {
   return (
     <div className='bg-white'>
-      
+      <Navbar></Navbar>
       <div className='pt-24 min-h-[calc(100vh-68px)]'>
         <Outlet />
       </div>
