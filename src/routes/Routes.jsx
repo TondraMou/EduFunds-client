7e9bfa8d055f463d56cf6af1c5f95bdccc765f03
TondraMou <tondramou3@gmail.com>
2025-01-25 16:36:58 +0600

    fixed routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,11 +2,23 @@ import { createBrowserRouter } from 'react-router-dom'
 import DashboardLayout from '../layouts/DashboardLayout'
 import MainLayout from '../layouts/MainLayout'
 import AddScholarship from '../pages/Dashboard/Moderator/AddScholarship'
+import Login from '../pages/Login/Login'
+import SignUp from '../pages/SignUp/SignUp'
 
 export const router = createBrowserRouter([
    {
     path: '/',
-    element: <MainLayout></MainLayout>
+    element: <MainLayout></MainLayout>,
+    children: [
+        {
+            path: '/login',
+            element: <Login></Login>
+        },
+        {
+            path: '/signup',
+            element: <SignUp></SignUp>
+        }
+    ]
    },
   {
     path: '/dashboard',
