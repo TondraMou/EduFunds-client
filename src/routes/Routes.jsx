36aa867d411f2c55bf0c99906d533f9033705382
TondraMou <tondramou3@gmail.com>
2025-01-25 06:20:46 +0600

    added login

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom'
 import DashboardLayout from '../layouts/DashboardLayout'
+import MainLayout from '../layouts/MainLayout'
 
 export const router = createBrowserRouter([
+   {
+    path: '/',
+    element: <MainLayout></MainLayout>
+   },
   {
     path: '/dashboard',
     element: <DashboardLayout></DashboardLayout>
